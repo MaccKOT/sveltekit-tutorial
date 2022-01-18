0d20922c6152fef01344191a0f77669485b30afa
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2022-01-18 15:14:46 +0400

    Layouts reset

--- src/routes/guides/index.svelte
+++ src/routes/guides/index.svelte
@@ -1,10 +1,10 @@
 <div class="guides">
   <ul>
     <li>
-      <a href="#">guide 1</a>
+      <a class="guides_link" href="#">guide 1</a>
     </li>
     <li>
-      <a href="#">guide 2</a>
+      <a class="guides_link" href="#">guide 2</a>
     </li>
   </ul>
 </div>
@@ -12,15 +12,21 @@
 <style>
   .guides {
     margin-top: 20px;
+    padding-left: 20px;
   }
   ul {
     list-style-type: none;
     padding: 0;
   }
-  a {
+  a.guides_link {
     display: inline-block;
     margin-top: 10px;
     padding: 10px;
     border: 1px dotted rgba(255, 255, 255, 0.2);
+    text-decoration: none;
+  }
+  a.guides_link:hover,
+  a.guides_link:focus {
+    border: 1px dotted rgba(255, 255, 255, 0.8);
   }
 </style>
