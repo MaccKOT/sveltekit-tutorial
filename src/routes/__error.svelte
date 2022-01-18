767a9e0b122d1bc5e26d85426705954290261672
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2022-01-18 16:41:24 +0400

    Custom error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+  export function load({ error }) {
+    return {
+      props: {
+        message: error.message,
+      },
+    };
+  }
+</script>
+
+<script>
+  export let message;
+</script>
+
+<!-- custom error page -->
+<div class="error">
+  <h2>Oops!</h2>
+  <p>{message}</p>
+  <a href="/guides">View all guides</a>
+  <a href="/">Return to main page</a>
+</div>
+
+<style>
+  .error {
+    text-align: center;
+    margin-bottom: 60px;
+  }
+</style>
