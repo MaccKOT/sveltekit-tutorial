4f0cdd2502702129a52fc58434bc58be66505dde
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2022-01-18 16:59:54 +0400

    Preparing for deploy to Netlify #2

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-//import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-netlify';
+//import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +7,7 @@ const config = {
     adapter: adapter({
       // if true, will split your app into multiple functions
       // instead of creating a single one for the entire app
-      split: false,
+      // split: false,
     }),
 
     // hydrate the <div id="svelte"> element in src/app.html
